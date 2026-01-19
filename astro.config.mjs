0e9bf2e09bdd58f034ebe98b8ee85bb0aa6c673c
Milán Boleradszki <bmilan1985@gmail.com>
2026-01-19 21:45:51 +0000

    remove hu locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,23 +7,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.bolyosaniko.com',
   base: '',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'hu'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
   integrations: [
     tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en-GB',
-          hu: 'hu-HU',
-        },
-      },
-    }),
+    sitemap(),
   ]
 });
